6103775434a4dfefb20e04a2df31737e5164538b
M.T. Oyelowo <oyelowomayowa@gmail.com>
2022-12-01 11:06:35 +0100

    First Commit

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,12 +2,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+    firstName: string
+    lastName: string
 }
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'M.T. Oyelowo' })
+    res.status(200).json({ firstName: 'Mayowa', lastName: 'Oyelowo' })
 }
