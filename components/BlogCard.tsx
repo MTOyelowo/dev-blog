fc1596123e3d89d3bd84585dc126934e4f0b53e3
M.T. Oyelowo <oyelowomayowa@gmail.com>
2022-12-02 22:33:53 +0100

    fixed cursor

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -11,8 +11,10 @@ const BlogCard: FC<Props> = ({ title, desc, slug }): JSX.Element => {
   return (
     <div className="bg-green-100 p-2 rounded-lg cursor-pointer">
       <Link href={"/blogs/" + slug}>
-        <h1 className="text-gray-900 text-3xl font-semibold">{title}</h1>
-        <p className="text-gray-500">{desc}.</p>
+        <h1 className="text-gray-900 text-3xl font-semibold cursor-pointer">
+          {title}
+        </h1>
+        <p className="text-gray-500 cursor-pointer">{desc}.</p>
       </Link>
     </div>
   );
